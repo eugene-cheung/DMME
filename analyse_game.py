c2fe873c10defe76482b4ecaeba6a097d844147e
danishubin <dshubin@eastsideprep.org>
2025-01-25 13:56:32 -0800

    connect analyse.game.py and main

--- analyse_game.py
+++ analyse_game.py
@@ -2,21 +2,26 @@ import chess
 import chess.pgn
 import requests
 import json
+from stockfish import Stockfish
 
+stockfish = Stockfish(path="stockfish/stockfish-windows-x86-64-avx2.exe")
 
 def readPGN(file):
+    # print(file)
     pgn = open(file)
     first_game = chess.pgn.read_game(pgn)
+    # print(first_game)
     return first_game
 
 def analyseGame(game):
+    # print(game)
     data = []
     board = game.board()
     for move in game.mainline_moves():
         board.push(move)
-        stockFishResults = stockfishAPI(board.fen(), 12)
+        stockFishResults = stockfishFile(board.fen(), 5)
         # board, eval, continuation, legal moves
-        data.append([board, stockFishResults[1], stockFishResults[2], getLegalMoves(board)])
+        data.append([board, stockFishResults[0], stockFishResults[1], getLegalMoves(board)])
     
     return data
 
@@ -25,14 +30,24 @@ def getLegalMoves(board):
     return board.legal_moves
 
 # https://stockfish.online/api/s/v2.php
-def stockfishAPI(fen, depth):
-    url = "https://stockfish.online/api/s/v2.php"
+# def stockfishAPI(fen, depth):
+#     url = "https://stockfish.online/api/s/v2.php"
 
-    response = requests.get(url+"?fen="+fen+"&depth=" + str(depth))
-    data = response.json()
+#     response = requests.get(url+"?fen="+fen+"&depth=" + str(depth))
+#     data = response.json()
 
-    if data['success'] == False:
-        print("There was an error with stockfish")
-        return
+#     if data['success'] == False:
+#         print("There was an error with stockfish")
+#         return
 
-    return [data['bestmove'], data['evaluation'], data['continuation']]
+#     return [data['bestmove'], data['evaluation'], data['continuation']]
+
+def stockfishFile(fen, depth):
+    stockfish.set_fen_position(fen)
+    stockfish.set_depth(depth)
+    # stockfish.is_move_correct('a2a3')
+
+    data = [stockfish.get_best_move(), stockfish.get_evaluation()]
+    return data
+
+# print(analyseGame(readPGN("lichess_pgn_2024.09.05_TigritoForever_vs_shubindani.1Ex3Xcl7 (2).pgn")))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 
+from analyse_game import readPGN, analyseGame
+
 app = Flask(__name__)
 Bootstrap(app)
 
@@ -13,6 +15,10 @@ def home():
 def analysis():
     pgn_data = request.form['pgn']
     return render_template("analysis.html")
+    pgn = readPGN("lichess_pgn_2024.09.05_TigritoForever_vs_shubindani.1Ex3Xcl7 (2).pgn")
+    data = analyseGame(pgn)
+    print(data)
+    return render_template("analysis.html", data = data)
 
 @app.route("/about")
 def about():
