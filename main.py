db848d2eee7a371e9483122e5825439b5a63594e
mpolasub <nondhawit@gmail.com>
2025-01-25 10:42:49 -0800

    more template files

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
