dcfcf8be58ff9787d1cbddf66ff9de46f04fd030
mpolasub <nondhawit@gmail.com>
2025-01-25 12:08:29 -0800

    added navbar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+Bootstrap5(app)
 
 @app.route("/")
 def home():
@@ -10,6 +12,9 @@ def home():
 def analysis():
     return render_template("analysis.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
