3bb62c2d43439956cd5c8558ef48f8fbc112d4a0
danishubin <dshubin@eastsideprep.org>
2025-01-25 12:32:08 -0800

    Analyse PGN

---
+++ analyse_game.py
@@ -0,0 +1,39 @@
+import chess
+import chess.pgn
+import requests
+import json
+
+
+def readPGN(file):
+    pgn = open(file)
+    first_game = chess.pgn.read_game(pgn)
+    return first_game
+
+def analyseGame(game):
+    data = []
+    board = game.board()
+    for move in game.mainline_moves():
+        board.push(move)
+        stockFishResults = stockfishAPI(board.fen(), 12)
+        # board, eval, continuation, complexity, legal moves
+        data.append([board, stockFishResults[1], stockFishResults[2], getLegalMoves(board)])
+    
+    return data
+
+
+def getLegalMoves(board):
+    return board.legal_moves
+
+# https://stockfish.online/api/s/v2.php
+def stockfishAPI(fen, depth):
+    url = "https://stockfish.online/api/s/v2.php"
+
+    response = requests.get(url+"?fen="+fen+"&depth=" + str(depth))
+    data = response.json()
+
+    if data['success'] == False:
+        print("There was an error with stockfish")
+        return
+
+    print(data)
+    return [data['bestmove'], data['evaluation'], data['continuation']]
