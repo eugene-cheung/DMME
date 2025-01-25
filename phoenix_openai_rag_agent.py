84009ef6e3270e6f5eaedb8b6c5d68c04ff51777
eugene-cheung <eugenus1@uw.edu>
2025-01-25 10:35:46 -0800

    some more starter files

--- phoenix_openai_rag_agent.py
+++ phoenix_openai_rag_agent.py
@@ -3,26 +3,35 @@
 
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 from dotenv import load_dotenv
 import os
+import phoenix as px
+from phoenix.trace.langchain import LangChainInstrumentor
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.callbacks import StdOutCallbackHandler
+from langchain.callbacks.arize_callback import ArizeCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain.tools.retriever import create_retriever_tool
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor, create_openai_tools_agent
 
 load_dotenv()
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-import phoenix as px
+
 session = px.launch_app()
 
-from phoenix.trace.langchain import LangChainInstrumentor
+
 LangChainInstrumentor().instrument()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
 loader = DirectoryLoader("../city_data")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
+
 
 embeddings = OpenAIEmbeddings()
 db = FAISS.from_documents(
@@ -31,11 +40,9 @@ db = FAISS.from_documents(
 
 retriever = db.as_retriever()
 
-from langchain_core.callbacks import StdOutCallbackHandler
-from langchain.callbacks.arize_callback import ArizeCallbackHandler
-from langchain.callbacks.manager import CallbackManager
 
-from langchain.tools.retriever import create_retriever_tool
+
+
 
 tool = create_retriever_tool(
     retriever,
@@ -44,16 +51,16 @@ tool = create_retriever_tool(
 )
 tools = [tool]
 
-from langchain import hub
+
 
 prompt = hub.pull("hwchase17/openai-tools-agent")
 prompt.messages
 
-from langchain_openai import ChatOpenAI
+
 
 llm = ChatOpenAI(temperature=0, verbose=True)
 
-from langchain.agents import AgentExecutor, create_openai_tools_agent
+
 
 agent = create_openai_tools_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools)
